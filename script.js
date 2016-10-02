61fe17cb3884982b264b1612c30196249027a468
Shi Hao Tan <shihaotan91@gmail.com>
2016-10-02 22:15:46 +0800

    added enemy2. used objects callback to update enemy

--- script.js
+++ script.js
@@ -34,6 +34,14 @@ spdY: 20,
 name: 'E',
 }
 
+var enemy2 = {
+x: 230,
+spdX :40,
+y: 100,
+spdY: 70,
+name: 'E',
+}
+
 
 //canvas size
 var HEIGHT = 400
@@ -41,46 +49,65 @@ var WIDTH = 700
 
 var message = 'bouncing'
 
-function updatePlayer(){
-  //player
-   player.x += player.spdX;
-   player.y += player.spdY;
-   ctx.fillText(player.name,player.x,player.y);
-   console.log('hello',player.x);
+// function updatePlayer(){
+//   //player
+//    player.x += player.spdX;
+//    player.y += player.spdY;
+//    ctx.fillText(player.name,player.x,player.y);
+//    console.log('hello',player.x);
+//
+//    if(player.x > WIDTH || player.x < 0){
+//    console.log(message);
+//    player.spdX = -player.spdX
+//    }
+//    if(player.y > HEIGHT || player.y < 0){
+//    console.log(message);
+//    player.spdY = -player.spdY
+//  }
+// }
 
-   if(player.x > WIDTH || player.x < 0){
-   console.log(message);
-   player.spdX = -player.spdX
-   }
-   if(player.y > HEIGHT || player.y < 0){
-   console.log(message);
-   player.spdY = -player.spdY
- }
-}
+// function updateEnemy(){
+//
+//    //enemy
+//    enemy.x += enemy.spdX;
+//    enemy.y += enemy.spdY;
+//    ctx.fillText(enemy.name,enemy.x,enemy.y);
+//    console.log('hello',enemy.x)
+//
+//    if(enemy.x > WIDTH || enemy.x < 0){
+//    console.log(message);
+//    enemy.spdX = -enemy.spdX
+//    }
+//    if(enemy.y > HEIGHT || enemy.y < 0){
+//    console.log(message);
+//    enemy.spdY = -enemy.spdY
+//   }
+// }
 
-function updateEnemy(){
+function updateObject(object){
 
    //enemy
-   enemy.x += enemy.spdX;
-   enemy.y += enemy.spdY;
-   ctx.fillText(enemy.name,enemy.x,enemy.y);
-   console.log('hello',enemy.x)
+   object.x += object.spdX;
+   object.y += object.spdY;
+   ctx.fillText(object.name,object.x,object.y);
+   console.log('hello',object.x)
 
-   if(enemy.x > WIDTH || enemy.x < 0){
+   if(object.x > WIDTH || object.x < 0){
    console.log(message);
-   enemy.spdX = -enemy.spdX
+   object.spdX = -object.spdX
    }
-   if(enemy.y > HEIGHT || enemy.y < 0){
+   if(object.y > HEIGHT || object.y < 0){
    console.log(message);
-   enemy.spdY = -enemy.spdY
+   object.spdY = -object.spdY
   }
 }
 
 function update(){
-updatePlayer();
-updateEnemy();
+updateObject(enemy);
+updateObject(enemy2);
+updateObject(player);
 }
 
 update();
 
-setInterval(update,40);
+setInterval(update,100);
